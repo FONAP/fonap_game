691751c1558e0c24a3734e105751cd29306949ac
toninow <abenalcazarc@gmail.com>
2021-02-03 08:54:21 -0500

    sound questions added

--- GameUnityFonap/questions/Assets/Scripts/QuizManager.cs
+++ GameUnityFonap/questions/Assets/Scripts/QuizManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class QuizManager : MonoBehavior
+public class QuizManager : MonoBehaviour
 {
     public List<QuestionAndAnswers> QnA;
     public GameObject[] options;
