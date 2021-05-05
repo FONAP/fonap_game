a447dc121d5eb239f69175f7d59fb60778bdb7f3
OsorioCarlos <carlosalexander.2001@hotmail.com>
2021-05-04 21:13:41 -0500

    Robber IA

---
+++ GameUnityFonap/Assets/Scene Risks/Scripts/Robber.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robber : MonoBehaviour
+{
+    public Transform tarjet;
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(tarjet.position);
+    }
+}
