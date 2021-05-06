a867e3930fc24ef0791ac6c5d3912af711291671
OsorioCarlos <carlosalexander.2001@hotmail.com>
2021-05-06 17:38:57 -0500

    Robber Automata

--- GameUnityFonap/Assets/Scene Risks/Scripts/Robber.cs
+++ GameUnityFonap/Assets/Scene Risks/Scripts/Robber.cs
@@ -7,6 +7,7 @@ public class Robber : MonoBehaviour
 {
     public Transform tarjet;
     private NavMeshAgent agent;
+    private float timer = 0f;
 
     void Awake()
     {
@@ -17,11 +18,22 @@ public class Robber : MonoBehaviour
     {
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        tarjet.position = new Vector2(Random.Range(-7, 26), Random.Range(-3, 14));
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        if (timer >= 10) ChangeTargetPosition();
+
         agent.SetDestination(tarjet.position);
+    }
+
+    void ChangeTargetPosition()
+    {
+        tarjet.position = new Vector2(Random.Range(-7, 26), Random.Range(-3, 14));
+        timer = 0f;
     }
 }
