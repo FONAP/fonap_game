ff9a19404eb4c9da4628f01a82e3e28ebf07f1e7
OsorioCarlos <carlosalexander.2001@hotmail.com>
2021-05-11 20:21:05 -0500

    Add Robber Animations

--- GameUnityFonap/Assets/Scene Risks/Scripts/Robber.cs
+++ GameUnityFonap/Assets/Scene Risks/Scripts/Robber.cs
@@ -7,33 +7,71 @@ public class Robber : MonoBehaviour
 {
     public Transform tarjet;
     private NavMeshAgent agent;
+    private Animator animator;
+
+    private const float movingDistance = 10f;
+
     private float timer = 0f;
+
+    private float newPositionX;
+    private float newPositionY;
+
+    private float minValue;
+    private float maxValue;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
     {
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        tarjet.position = new Vector2(Random.Range(-7, 26), Random.Range(-3, 14));
+
+        maxValue = Mathf.Max(-7, transform.position.x - movingDistance);
+        minValue = Mathf.Min(26, transform.position.x + movingDistance);
+        newPositionX = Random.Range(maxValue, minValue);
+
+        maxValue = Mathf.Max(-3, transform.position.y - movingDistance);
+        minValue = Mathf.Min(14, transform.position.y + movingDistance);
+        newPositionY = Random.Range(maxValue, minValue);
+
+        tarjet.position = new Vector2(newPositionX, newPositionY);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= 10) ChangeTargetPosition();
-
         agent.SetDestination(tarjet.position);
+
+        if (agent.remainingDistance > agent.stoppingDistance) animator.SetBool("is_moving", true);
+        else WaitState();
+
+        if (timer >= 3) ChangeTargetPosition();
+
     }
 
     void ChangeTargetPosition()
     {
-        tarjet.position = new Vector2(Random.Range(-7, 26), Random.Range(-3, 14));
+        maxValue = Mathf.Max(-7, transform.position.x - movingDistance);
+        minValue = Mathf.Min(26, transform.position.x + movingDistance);
+        newPositionX = Random.Range(maxValue, minValue);
+
+        maxValue = Mathf.Max(-3, transform.position.y - movingDistance);
+        minValue = Mathf.Min(14, transform.position.y + movingDistance);
+        newPositionY = Random.Range(maxValue, minValue);
+
+        tarjet.position = new Vector2(newPositionX, newPositionY);
+
         timer = 0f;
+    }
+
+    void WaitState()
+    {
+        animator.SetBool("is_moving", false);
+        timer += Time.deltaTime;
     }
 }
